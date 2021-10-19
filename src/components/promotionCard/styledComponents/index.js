9445a7d670b644477fdbe2497006b5c876dc55b2
Akho Tshweza <akho.tshweza@sovtech.com>
2021-10-19 13:56:33 +0200

    update css

--- src/components/promotionCard/styledComponents/index.js
+++ src/components/promotionCard/styledComponents/index.js
@@ -4,15 +4,17 @@ import { calculateRem } from '../../../utils/helpers';
 const StyledPromotionCard = styled.div``;
 const StyledResponsivePromo = styled.div`
   position: relative;
-  height: 726px;
+  height:100%;
   img {
-    height: 726px;
+    width:100%;
+    height:100%;
   }
 `;
 const StyledResponsiveCaption = styled.div`
   z-index: 5;
   position: absolute;
   bottom: 0;
+  width:100%;
   background-color: rgba(0, 0, 0, 0.75);
   button {
     height: ${calculateRem(50)};
